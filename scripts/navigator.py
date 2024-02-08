3d7dd1b7b98a04ed611edda46ec27075c83b0e16
vboxuser <vboxuser@Ubuntu20.04.myguest.virtualbox.org>
2024-02-07 16:08:32 -0800

    Make some small changes to navigator

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -67,10 +67,10 @@ class Navigator:
         self.plan_start = [0.0, 0.0]
 
         # Robot limits
-        self.v_max = 0.5  # maximum velocity
-        self.om_max = 1  # maximum angular velocity
+        self.v_max = 0.25  # maximum velocity
+        self.om_max = .4  # maximum angular velocity
 
-        self.v_des = 0.3  # desired cruising velocity
+        self.v_des = 0.2  # desired cruising velocity
         self.theta_start_thresh = 0.05  # threshold in theta to start moving forward when path-following
         self.start_pos_thresh = (
             0.2  # threshold to be far enough into the plan to recompute it
