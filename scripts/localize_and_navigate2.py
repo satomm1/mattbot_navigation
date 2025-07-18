abb92b9570a18d79e348e223fed82bb767cc9d7a
Matthew Sato <satomm@stanford.edu>
2025-07-18 13:58:43 -0700

    fix radius->diameter

--- scripts/localize_and_navigate2.py
+++ scripts/localize_and_navigate2.py
@@ -1221,7 +1221,7 @@ class Navigator:
                     for (x, y) in self.person_list1 + self.person_list2 + self.person_list3:
                         obj_x.append(x)
                         obj_y.append(y)
-                        obj_d.append(0.3)  # Assuming a radius of 0.3m for people
+                        obj_d.append(0.3)  # Assuming a diameter of 0.3m for people
 
                     self.replan(obj_x=obj_x, obj_y=obj_y, obj_d=obj_d)
                 elif self.distance_to_person > PERSON_STOP_DISTANCE:
