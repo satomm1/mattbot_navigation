be0da1636123973aafb593a49bff422cc855b9e3
Matthew Sato <satomm@stanford.edu>
2026-06-26 00:07:31 +0000

    Remove filtering for 'cone' class in detected objects callback

--- scripts/occupancy_grid_mapper.py
+++ scripts/occupancy_grid_mapper.py
@@ -610,9 +610,6 @@ class Map:
             if obj.class_name == "unknown" or obj.class_name == "person":
                 continue
 
-            if obj.class_name != "cone":
-                continue
-
             # Check if the object already exists in the detected objects
             already_exists = False
             x = obj.pose.position.x
